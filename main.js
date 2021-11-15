a58ce12949ca1270eb91f400a90be356f40c049d
Alvaro Lopez Garcia <aloga@ifca.unican.es>
2021-11-15 16:11:20 +0100

    Install tox

--- main.js
+++ main.js
@@ -155,7 +155,7 @@ async function main() {
             "exec",
             container,
             "bash", "-c",
-            `apt-get install -yq -t '${imageTag}' dpkg-dev debhelper devscripts lintian python3-pip || apt-get install -yq dpkg-dev debhelper devscripts lintian python3-pip`
+            `apt-get install -yq -t '${imageTag}' dpkg-dev debhelper devscripts lintian python3-pip tox || apt-get install -yq dpkg-dev debhelper devscripts lintian python3-pip tox`
         ])
         core.endGroup()
 
