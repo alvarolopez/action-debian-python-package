359ca03120215b7448f6a4062a28a57836e16ef2
Alvaro Lopez Garcia <aloga@ifca.unican.es>
2021-11-15 16:11:20 +0100

    Install tox

--- main.js
+++ main.js
@@ -126,7 +126,7 @@ async function main() {
         await exec.exec("docker", [
             "exec",
             container,
-            "apt-get", "install", "-yq", "-t", imageTag, "dpkg-dev", "debhelper", "devscripts", "python3-pip"
+            "apt-get", "install", "-yq", "-t", imageTag, "dpkg-dev", "debhelper", "devscripts", "python3-pip", "tox"
         ])
         core.endGroup()
 
